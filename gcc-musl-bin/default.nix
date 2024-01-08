6b2abbc5ffaabb6a16e0cd8794036f723669f6dd
YizhePKU <yizhe@pku.edu.cn>
2024-01-08 04:14:59 +0000

    Remove binary file from gcc-musl-bin

--- gcc-musl-bin/default.nix
+++ gcc-musl-bin/default.nix
@@ -4,7 +4,10 @@ derivation {
   name = "gcc-musl-bin";
   system = "x86_64-linux";
   builder = ./builder.sh;
-  src = ./x86_64-linux-musl-native;
+  src = fetchTarball {
+    url = https://musl.cc/x86_64-linux-musl-native.tgz;
+    sha256 = "140gi72svsgs1qp94mh47763iv62qgpv5p0z9jsr10rnb0hc9273";
+  };
   __contentAddressed = true;
 
   busybox = busybox-bin;
