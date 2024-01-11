15cb5b023dfa6c9f84f2ad847a0cbe25e05734df
YizhePKU <yizhe@pku.edu.cn>
2024-01-11 05:45:17 +0000

    Package clang-stage1

---
+++ clang-stage1/builder.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH="$gcc/bin:$busybox/bin:$make/bin:$cmake/bin:$python3/bin:$git/bin"
+export LDFLAGS="-Wl,--dynamic-linker=$gcc/lib/libc.so -Wl,--enable-new-dtags -Wl,--rpath=$gcc/lib"
+
+cmake -S $src/clang \
+      -B $TMPDIR/build \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$out \
+      -DLLVM_TARGETS_TO_BUILD=X86 \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_ROOT=$llvm \
+
+cmake --build $TMPDIR/build -j8
+cmake --install $TMPDIR/build
---
+++ clang-stage1/default.nix
@@ -0,0 +1,20 @@
+{ busybox-bin, gcc-musl-bin, make-tmp, cmake-tmp, python3-tmp, git-tmp, llvm-stage1 }:
+
+derivation {
+  name = "clang-17.0.6";
+  system = "x86_64-linux";
+  builder = ./builder.sh;
+  src = fetchTarball {
+    url = https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz;
+    sha256 = "1a7rq3rgw5vxm8y39fyzr4kv7w97lli4a0c1qrkchwk8p0n07hgh";
+  };
+  __contentAddressed = true;
+
+  busybox = busybox-bin;
+  gcc = gcc-musl-bin;
+  make = make-tmp;
+  cmake = cmake-tmp;
+  python3 = python3-tmp;
+  git = git-tmp;
+  llvm = llvm-stage1;
+}
